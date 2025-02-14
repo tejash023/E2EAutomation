f209bf2ece1babdf9febdbea3aeafe634bc2d1ec
Ankit Anand <anandankit448@gmail.com>
2025-02-14 23:39:39 +0530

    Change of Class TC04 to TC06, new class addition

---
+++ src/test/java/testCases/TC05_LogoutUser.java
@@ -0,0 +1,47 @@
+package testCases;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+import pageObjects.LoginSignupPage;
+
+public class TC05_LogoutUser extends BaseClass{
+    /*
+        / Test Steps to be followed for Successful Login
+        / Click on 'Signup / Login' button
+        / Verify 'Login to your account' is visible
+        / Enter correct email address and password
+        / Click 'login' button
+        / Verify that 'Logged in as username' is visible
+        / Verify Logout Button is visible
+        / Click 'Logout' button
+        */
+    @Test(groups = {"Master", "Regression"})
+    public void login()
+    {
+        logger.info("**** Successful Login Test Start ****");
+        try {
+            HomePage homePage=new HomePage(driver);
+            homePage.clickMenuItem("Login");
+            LoginSignupPage loginSignupPage=new LoginSignupPage(driver);
+
+            logger.info("**** Email and Password Taken from Properties file ****");
+
+            loginSignupPage.setTextLoginEmail(p.getProperty("email"));
+            loginSignupPage.setTxtPassword(p.getProperty("password"));
+            loginSignupPage.clickLoginButton();
+            HomePage homePage1=new HomePage(driver);
+            WebElement loggedInAs=homePage1.getMenuElement("Logged in as"+" abc");
+            Assert.assertTrue(loggedInAs.isDisplayed(),"loggedInAs is not displayed");
+            Assert.assertTrue(homePage1.getMenuElement("Logout").isDisplayed());
+            homePage1.getMenuElement("Logout").click();
+        } catch (Exception e) {
+            logger.error("TC02 Test Failed");
+            logger.debug("Debug Logs");
+            Assert.fail("TC02 Test Failed");
+        }
+        logger.info("**** TC02_Successful Login Completed ****");
+
+    }
+}
--- src/test/java/testCases/TC06_ContactForm.java
+++ src/test/java/testCases/TC06_ContactForm.java
@@ -2,7 +2,7 @@ package testCases;
 
 import org.testng.annotations.Test;
 
-public class TC04_ContactForm extends BaseClass{
+public class TC06_ContactForm extends BaseClass{
 
     @Test(groups = "Master")
     public void contactFormTest(){
